90e4eff97b36691ac9dd0b06fa22264b4a8a945c
vhundef <vhundef@outlook.com>
2019-03-20 20:23:03 +0300

    Lab1_2 done on easy

--- lab1/lab1_2.cpp
+++ lab1/lab1_2.cpp
@@ -1,12 +1,16 @@
-//
-// Created by vhundef on 18.02.19.
-//
+/***
+ * @author Vladimir Shubarin.
+ * @date 19.03.19
+ * @file lab1_2.cpp
+*/
 
+#include <cstdlib>
 #include <iostream>
-
-typedef int DataType;
+#include <random>
 
 using namespace std;
+
+typedef int DataType;
 
 class Queue {
     struct element {
@@ -14,28 +18,71 @@ class Queue {
         element *next; ///< IDK what this var does
         double coef; ///< Коэф c в (cx^en)
         int expon; ///< Степень x
-    } *front, *rear; //индексы головы и хвоста
+    }
+            *front, *rear; //индексы головы и хвоста
 public:
     Queue() { front = rear = nullptr; }
-
     ~Queue();        ///<деструктор (освобождение памяти)
     int Empty(); ///<проверка на пустоту
     int Full(); ///<проверка на полноту заполнения
-    DataType Front(); ///<неразрушающее чтение элемента
-    int EnQueue(DataType x); ///<добавление элемента в очередь
-    DataType DeQueue(); ///<извлечение элемента из очереди
+    int Front(); ///<неразрушающее чтение элемента
+    int EnQueue(int x); ///<добавление элемента в очередь
+    int EnQueue(int x, int coef, int powr); ///<@brief добавление элемента в очередь
+    int DeQueue(); ///<извлечение элемента из очереди
 };
 
-int main() {
+
+void clearBuff() {
+    cin.clear();    // Restore input stream to working state
+    cin.ignore(100, '\n');    // Get rid of any garbage that user might have entered}
+}
+/// @param q Указатель на очередь
+/// @param qIndex  ?
+/// @param amountOfRecords Количество рандомногенерируемых записей
+int fillFileWithRandomData(int qIndex, Queue * q, int amountOfRecords);
+
+int main(int argc, char *argv[]) {
+    setlocale(LC_CTYPE, "rus");
+    char menu = '0';
     Queue q;
     int i = 0;
-    while (q.EnQueue(i)) {
-        i++;
+    while (menu != '4') {
+        cout << "1. Добавить элемент" << endl;
+        cout << "2. Посчитать diff(NOT YET IMPLEMENTED)" << endl;
+        cout << "3. Show" << endl;
+        cout << "4. Quit" << endl;
+        cout << "5. Заполнить рандомно" << endl;
+        cin >> menu;
+        clearBuff();
+        switch (menu) {
+            case '1':
+                // system("clear");
+                q.EnQueue(i);
+                i++;
+                break;
+            case '2':
+                // system("clear");
+                throw;
+                break;
+            case '3':
+                while (!q.Empty()) {
+                    q.DeQueue();
+                }
+                cout << endl;
+                break;
+            case '5':
+                int tmp;
+                cout<<"Количесто рандомных элементов: ";
+                cin>>tmp;
+                fillFileWithRandomData(i,&q,tmp);
+                break;
+            default:
+                break;
+        }
     }
-    while (!q.Empty())
-        cout << q.DeQueue();
     return 0;
 }
+
 
 int Queue::Empty() {
     return front == nullptr;
@@ -74,13 +121,6 @@ int Queue::EnQueue(DataType x) {
     return 1;
 }
 
-DataType Queue::DeQueue() {
-    DataType temp = front->data;
-    element *tmp = front;
-    front = front->next;
-    delete tmp;
-    return temp;
-}
 
 Queue::~Queue() {
     element *temp = front;
@@ -89,4 +129,51 @@ Queue::~Queue() {
         front = front->next;
         delete temp;
     }
+}
+
+/// @brief Somewhy returns element index... not that it is useful in this case
+DataType Queue::DeQueue() {
+    DataType temp = front->data;
+    element *tmp = front;
+    tmp->coef;///< Пример получения коэф.-та
+    tmp->expon;///< Пример получения показателя степени
+    cout << tmp->coef << "*x^" << tmp->expon;
+    front = front->next;
+    delete tmp;
+    if (front != nullptr) cout << "+";
+    return temp;
+}
+
+int Queue::EnQueue(DataType x, int coef, int powr) { ///< @warning Only for debug purposes
+    element *temp = new(std::nothrow) element;
+    if (temp == nullptr) return 1;
+    temp->data = x;
+    temp->next = nullptr;
+    if (front == nullptr)
+        front = rear = temp;
+    else {
+        rear->next = temp;
+        rear = rear->next;
+    }
+    temp->coef = coef;
+    temp->expon = powr;
+    if (temp->expon < 0) {
+        return 0;
+    }
+    return 1;
+}
+
+int randomNum() { ///< @brief Генерирует случайные числа от 0 до 100 @bug на windows не работает
+    random_device dev;
+    mt19937 rng(dev());
+    uniform_int_distribution<mt19937::result_type> dist6(1, 100);
+    return static_cast<int>(dist6(rng));
+}
+
+int fillFileWithRandomData(int qIndex, Queue * q, int amountOfRecords) {
+    int i;
+    for (i = qIndex; i < amountOfRecords; i++) {
+        q->EnQueue(qIndex, randomNum(), randomNum());
+    }
+    return i;
 }
