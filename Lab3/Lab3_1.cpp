37a0f9a9ee388375540195675755eb6c8e9ae448
Vladimir Shubarin <vhundef@gmail.com>
2019-05-22 00:47:24 +0300

    lab3_1 Updated added InsertSort added BinInsertSort

---
+++ Lab3/Lab3_1.cpp
@@ -0,0 +1,281 @@
+// По возрастанию
+
+//N1=15000
+//N2=40000
+//N3=80000
+//N4=150000
+
+
+#include <fstream>
+#include <iostream>
+#include "lab_3_1_sort.h"
+
+using namespace std;
+int main(){
+	Lab3_1_Sort sort;
+
+
+
+	int *arr,tmp,i=0;
+
+	cout<<"=============\nСортировка простыми вставками\n=============\n";
+	//For N1
+	arr=new int[15000];
+
+	cout<<"N1 15000 элементов"<<endl;
+
+	ifstream infile("unsorted.txt");
+	while ((infile >> tmp)&&i<=15000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	sort.insertionSort(arr,15001);
+	i=0;
+	while ((infile >> tmp)&&i<=15000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	sort.insertionSort(arr,15001);
+	i=0;
+	while ((infile >> tmp)&&i<=15000){
+		arr[i]=tmp;
+		i++;
+	}
+	infile.close();
+
+	sort.insertionSort(arr,15001);
+
+	delete(arr);
+
+	//For N2
+
+	cout<<"\n____________\nN2 40000 элементов"<<endl;
+	infile.open("unsorted.txt");
+
+	arr=new int[40000];
+	i=0;
+
+	while ((infile >> tmp)&&i<=40000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	sort.insertionSort(arr,40001);
+	i=0;
+	while ((infile >> tmp)&&i<=40000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	sort.insertionSort(arr,40001);
+	i=0;
+	while ((infile >> tmp)&&i<=40000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	infile.close();
+
+	sort.insertionSort(arr,40001);
+
+	delete(arr);
+
+	//For N3
+
+	cout<<"\n____________\nN3 80000 элементов"<<endl;
+	infile.open("unsorted.txt");
+
+	arr=new int[80000];
+	i=0;
+
+	while ((infile >> tmp)&&i<=80000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	sort.insertionSort(arr,80001);
+	i=0;
+	while ((infile >> tmp)&&i<=80000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	sort.insertionSort(arr,80001);
+	i=0;
+	while ((infile >> tmp)&&i<=80000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	infile.close();
+
+	sort.insertionSort(arr,80001);
+
+	delete(arr);
+
+	//For N4
+
+	cout<<"\n____________\nN4 150000 элементов"<<endl;
+	infile.open("unsorted.txt");
+
+	arr=new int[150000];
+	i=0;
+
+	while ((infile >> tmp)&&i<=150000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	sort.insertionSort(arr,150001);
+	i=0;
+	while ((infile >> tmp)&&i<=150000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	sort.insertionSort(arr,150001);
+	i=0;
+	while ((infile >> tmp)&&i<=150000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	infile.close();
+
+	sort.insertionSort(arr,150001);
+
+	delete(arr);
+
+
+/* THE FOLLOWING CODE WILL CAUSE EXCEPTION DO NOT USE IT
+	cout<<"\n=============\nСортировка бинарными вставками\n=============\n";
+	//For N1
+	arr=new int[15000];
+
+	cout<<"N1 15000 элементов"<<endl;
+
+	infile.open("unsorted.txt");
+	while ((infile >> tmp)&&i<=15000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	sort.binInsertionSort(arr,15000);
+	i=0;
+	while ((infile >> tmp)&&i<=15000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	sort.binInsertionSort(arr,15000);
+	i=0;
+	while ((infile >> tmp)&&i<=15000){
+		arr[i]=tmp;
+		i++;
+	}
+	infile.close();
+
+	sort.binInsertionSort(arr,15000);
+
+	//For N2
+
+	cout<<"\n____________\nN2 40000 элементов"<<endl;
+	infile.open("unsorted.txt");
+
+	arr=new int[40000];
+	i=0;
+
+	while ((infile >> tmp)&&i<=40000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	sort.binInsertionSort(arr,40000);
+	i=0;
+	while ((infile >> tmp)&&i<=40000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	sort.binInsertionSort(arr,40000);
+	i=0;
+	while ((infile >> tmp)&&i<=40000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	infile.close();
+
+	sort.binInsertionSort(arr,40000);
+
+
+
+	//For N3
+
+	cout<<"\n____________\nN3 80000 элементов"<<endl;
+	infile.open("unsorted.txt");
+
+	arr=new int[80000];
+	i=0;
+
+	while ((infile >> tmp)&&i<=80000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	sort.binInsertionSort(arr,80000);
+	i=0;
+	while ((infile >> tmp)&&i<=80000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	sort.binInsertionSort(arr,80000);
+	i=0;
+	while ((infile >> tmp)&&i<=80000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	infile.close();
+
+	sort.binInsertionSort(arr,80000);
+
+
+	//For N4
+
+	cout<<"\n____________\nN4 150000 элементов"<<endl;
+	infile.open("unsorted.txt");
+
+	arr=new int[150000];
+	i=0;
+
+	while ((infile >> tmp)&&i<=150000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	sort.binInsertionSort(arr,150000);
+	i=0;
+	while ((infile >> tmp)&&i<=150000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	sort.binInsertionSort(arr,150000);
+	i=0;
+	while ((infile >> tmp)&&i<=150000){
+		arr[i]=tmp;
+		i++;
+	}
+
+	infile.close();
+
+	sort.binInsertionSort(arr,150000);
+
+
+	return 0;
+ */
+}
---
+++ Lab3/lab_3_1_sort.h
@@ -0,0 +1,98 @@
+//
+// Created by vhundef on 21.05.19.
+//
+
+#ifndef UNIVERSITY_DATA_ORGANISATION_AND_STUCTURES_LAB_3_1_SORT_H
+#define UNIVERSITY_DATA_ORGANISATION_AND_STUCTURES_LAB_3_1_SORT_H
+
+
+#include <chrono>
+
+class Lab3_1_Sort {
+public:
+	void insertionSort(int *, int);
+	void binInsertionSort(int *, int);
+
+private:
+	std::chrono::time_point<std::chrono::high_resolution_clock> t_start;
+	std::chrono::time_point<std::chrono::high_resolution_clock> t_end;
+
+
+	void setTStart();
+
+	void setTEnd();
+
+	double calcTimeDiffInMs(){
+		return std::chrono::duration<double, std::milli>(t_end-t_start).count();
+	}
+};
+
+void Lab3_1_Sort::insertionSort(int *arrayPtr, int length) {
+	setTStart();
+	int temp, // временная переменная для хранения значения элемента сортируемого массива
+			item; // индекс предыдущего элемента
+	for (int counter = 1; counter < length; counter++) {
+		temp = arrayPtr[counter]; // инициализируем временную переменную текущим значением элемента массива
+		item = counter - 1; // запоминаем индекс предыдущего элемента массива
+		while (item >= 0 &&
+		       arrayPtr[item] > temp) // пока индекс не равен 0 и предыдущий элемент массива больше текущего
+		{
+			arrayPtr[item + 1] = arrayPtr[item]; // перестановка элементов массива
+			arrayPtr[item] = temp;
+			item--;
+		}
+	}
+	setTEnd();
+	std::cout<<"Сортировка заняла "<<calcTimeDiffInMs()<<" мс"<<std::endl;
+	//return ;
+
+}
+void Lab3_1_Sort::binInsertionSort (int* data, int size){
+	setTStart();
+	int i;
+	for (i = 0; i < size; i++) {
+		int pos = -1;
+		int start = 0;
+		int end = i - 1;
+		int numToInsert = data[i];
+		// Находим место вставки с помощью бинарного поиска
+		while (start <= end && pos == -1) {
+			int middle = (start + end) / 2;
+			if (numToInsert > data[middle]) {
+				start = middle + 1;
+			} else if (numToInsert < data[middle]) {
+				end = middle - 1;
+			} else {
+				pos = middle;
+			}
+		}
+		if(end < 0){
+			// определяем позицию в случае если элемент меньше всех отсортированных
+			pos = 0;
+		} else if(start >= i){
+			// определяем позицию в случае если элемент больше всех отсортированных
+			pos = i;
+		}
+		if (pos < i) {
+			// сдвигаем элементы вправо на одну позицию
+			int j;
+			for (j = i; j > pos; j--) {
+				data[j] = data[j - 1];
+			}
+			data[pos] = numToInsert;
+		}
+	}
+	setTEnd();
+	calcTimeDiffInMs();
+}
+
+void Lab3_1_Sort::setTStart() {
+	t_start = std::chrono::high_resolution_clock::now();
+}
+
+void Lab3_1_Sort::setTEnd() {
+	t_end = std::chrono::high_resolution_clock::now();
+}
+
+
+#endif //UNIVERSITY_DATA_ORGANISATION_AND_STUCTURES_LAB_3_1_SORT_H
